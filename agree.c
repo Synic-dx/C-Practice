44389b465e8ed48725a9d9e77dccb623fa214a28
Synic-dx <shinjan1280@gmail.com>
2024-07-10 18:52:30 +0530

    agree

---
+++ agree.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char c = get_char("Amai biya korba? (y/n)\n ");
+
+    if (c == 'y' || c == 'Y')
+    {
+        printf("Cholo kori\n ");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf("ok goodbye\n ");
+    }
+    else {
+        printf("Ayein?\n ");
+    }
+}
